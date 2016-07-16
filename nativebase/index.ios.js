7d93729f41844dc19839b06f55c244c4869f11ee
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-07-17 00:55:34 +0700

    show icon on navbar

--- nativebase/index.ios.js
+++ nativebase/index.ios.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import {AppRegistry, Text} from 'react-native';
-import { Container, Header, Title, Content } from 'native-base';
+import {AppRegistry, Text } from 'react-native';
+import { Container, Header, Title, Content, Button, Icon} from 'native-base';
+
 
 export default class NativeBase extends Component {
   render() {
     return (
       <Container>
+
         <Header>
-          <Title>Header Na</Title>
+          <Button transparent>
+            <Icon name='ios-arrow-back'  size={30} color="blue"/>
+          </Button>
+          <Title>Header</Title>
+          <Button transparent>
+            <Icon name="ios-menu" size={30} color="blue" />
+          </Button>
         </Header>
 
+
+
         <Content>
-          <Text> This is content section </Text>
+          <Text> This is Content</Text>
         </Content>
+
       </Container>
     )
   }
