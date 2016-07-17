cce9a989addc996242411b8f44c46d75dea6a4ef
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-07-17 10:06:57 +0700

    regular expression

---
+++ elixir/14.exs
@@ -0,0 +1,7 @@
+IO.inspect Regex.run ~r{[aeiou]}, "caterpillar"
+
+IO.inspect Regex.scan ~r{[aeiou]}, "caterpillar"
+
+IO.inspect Regex.split ~r{[aeiou]}, "caterpillar"
+
+IO.inspect Regex.replace ~r{[aeiou]},  "caterpillar", "*"
