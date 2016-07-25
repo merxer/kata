7b8d9ae9caf9a2ee1edc753e8cdee792764f4859
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-07-25 08:55:44 +0700

    docs

---
+++ learnelixir/4.ex
@@ -0,0 +1,19 @@
+defmodule Math do
+  @docmodule """
+  Math Operator
+  """
+
+  @doc """
+  Adds two integers togeter.
+
+  ## Examples
+    add(1,2)
+    3
+
+    add(5,5)
+    10
+  """
+  def add(a, b) do
+    a + b
+  end
+end
