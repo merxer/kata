22c80bdcd60b5c740f36752c4f7b733faa36e4fa
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-07-27 17:28:03 +0700

    react, react-native meteor

---
+++ meteor/RNApp/index.android.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import App from './app';
+
+
+class RNApp extends Component {
+  render() {
+    return <App />
+  }
+}
+
+AppRegistry.registryComponent('RNApp', () => RNAapp);
