97c09b4733f40ab87a44770896272a5eebe87b1b
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-07-28 10:33:42 +0700

    pass by reference

---
+++ go/17.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Artist struct {
+  Name, Genre string
+  Songs int
+}
+
+func newRelease(a *Artist) int {
+  a.Songs++
+  return a.Songs
+}
+
+func main() {
+  me := &Artist{Name: "Matt", Genre:  "Electro", Songs: 42}
+
+  fmt.Printf("%s relaseed their %dth songs\n", me.Name, newRelease(me))
+  fmt.Printf("%s has a total of %d songs", me.Name, me.Songs)
+}
