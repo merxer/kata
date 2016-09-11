bd4c0850c82fd8987c62bfb6eaee657270e06223
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-09-11 09:20:56 +0700

    real numbers

---
+++ csharp/2.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Types {
+  static void Main() {
+    float floatPI = 3.14f;
+    Console.WriteLine(floatPI);
+
+    double doublePI = 3.14;
+    Console.WriteLine(doublePI);
+
+    double nan = Double.NaN;
+    Console.WriteLine(nan);
+
+   double infinity = Double.PositiveInfinity;
+   Console.WriteLine(infinity); 
+  }
+}
