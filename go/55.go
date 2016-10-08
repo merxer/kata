171fd326e8c5095c40a3cac3ef92d0aa4de23dd7
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-10-08 08:36:35 +0700

    slice

---
+++ go/55.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  mySlice := []int{2,3,5,7,11,13}
+  fmt.Println(mySlice)
+
+  fmt.Println(mySlice[1:4])
+
+  fmt.Println(mySlice[:3])
+
+  fmt.Println(mySlice[4:])
+}
