6b01428d2c5c4222b39e077e7f87b4c08ff2f7c6
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-10-09 12:02:03 +0700

    methods

---
+++ go/130.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "math"
+
+type Circle struct {
+  x, y, r float64
+}
+
+func (c *Circle) area() float64 {
+  return math.Pi * c.r * c.r
+}
+
+func main() {
+  c := Circle{0,0,5}
+  fmt.Println(c.area())
+
+}
