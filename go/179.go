13af22bdfd89f2df089f19c4633403821efbd6a5
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-10-22 20:58:10 +0700

    functions

---
+++ go/179.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(message("world"))
+}
+
+func message(name string) (message string) {
+	message = fmt.Sprintf("hello %v", name)
+	return message
+}
