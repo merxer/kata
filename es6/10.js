8462dd1fb48b94bfa781fb26ff95e4312affb110
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-10-26 09:41:14 +0700

    filters

---
+++ es6/10.js
@@ -0,0 +1,40 @@
+var users = [
+  {
+    "id": 1000,
+    "name": "Corey Griffith",
+    "username": "coGriffith",
+    "gender": "male",
+    "age": 13
+  },
+  {
+    "id": 1001,
+    "name": "Marion McDaniel",
+    "username": "mMcDaniel",
+    "gender": "female",
+    "age": 15
+  },
+  {
+    "id": 1002,
+    "name": "Tom Robbins",
+    "username": "tRobbins",
+    "gender": "male",
+    "age": 15
+  },
+  {
+    "id": 1003,
+    "name": "Ruth Harvey",
+    "username": "rHarvey",
+    "gender": "female",
+    "age": 14
+  },
+  {
+    "id": 1004,
+    "name": "Terry Willis",
+    "username": "tWillis",
+    "gender": "male",
+    "age": 13
+  }
+]
+
+filter_users = users.filter(user => user.gender === 'female')
+console.log(filter_users)
