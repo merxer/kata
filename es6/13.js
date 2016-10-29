d451d97e859d64514a1ad8230379e3d0754b1895
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-10-29 17:03:29 +0700

    filter

---
+++ es6/13.js
@@ -0,0 +1,17 @@
+var emojis = [
+  "🍺",
+  "🍻",
+  "🍶",
+  "🍵",
+  "☕️",
+  "🍼",
+  "💻",
+  "👙",
+  "🐶",
+  "🎮",
+  "💪"
+]
+
+
+filter_emoji = emojis.filter(emo => emo === "💪")
+console.log(filter_emoji)
