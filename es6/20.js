31189134a1a08be7436dafbc39f62f48293c7498
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-10-30 07:33:36 +0700

    find

---
+++ es6/20.js
@@ -0,0 +1,16 @@
+var emojis = [
+  "🍺",
+  "🍻",
+  "🍶",
+  "🍵",
+  "☕️",
+  "🍼",
+  "💻",
+  "👙",
+  "🐶",
+  "🎮",
+  "💪"
+]
+
+find_emoji = emojis.find(emoji => emoji === "🐶");
+console.log(find_emoji);
