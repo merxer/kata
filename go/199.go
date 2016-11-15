7fc7f1077097bfae4c24eb133bf14c4153552821
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-11-15 14:19:45 +0700

    vars

---
+++ go/199.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Metalloids"
+	var triple = [3]int{5, 14, 84}
+	elements := []string{"Boron", "Silicon", "Polonium"}
+	isMetal := false
+
+	fmt.Println(name,
+		triple,
+		elements,
+		isMetal)
+}
