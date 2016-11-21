d1b4b76e88504989ea5ceb5235ffdf43a4ae19d9
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-11-21 21:23:16 +0700

    kata

---
+++ go/233.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var (
+	goIsFun bool = true
+	name string = "Pat"
+)
+
+func main(){
+	const format = "%T(%v)\n"
+	fmt.Printf(format, goIsFun, goIsFun)
+	fmt.Printf(format, name, name)
+}
