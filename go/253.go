93978b88ba760df00e5b9221d8ad54df64298b86
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-11-22 15:46:06 +0700

    kata

---
+++ go/253.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	cities := []string{}
+	cities = append(cities, "San Diego", "Bangkok")
+
+	fmt.Printf("%q\n", cities)
+	fmt.Println(len(cities))
+
+	countries := make([]string, 42)
+	fmt.Println(len(countries))
+}
