2b5baf50a313870df7f77f418c9b6d9b17b46a1d
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-11-23 11:29:33 +0700

    code organization

---
+++ go/282.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+const (
+	ConstExample = "Constant"
+)
+
+var (
+	universityName = "CMRU"
+	room = 264
+)
+
+type Student struct {
+	Firstname, Lastname string
+	universityName string
+}
+
+func (s *Student) print() {
+	fmt.Println(s.Firstname, s.Lastname, s.universityName)
+}
+
+
+func main() {
+	 u := &Student{
+		 Firstname: "THAWATCHAI",
+		 Lastname: "SINGNGAM",
+		 universityName: universityName,
+	 }
+
+	 u.print()
+}
