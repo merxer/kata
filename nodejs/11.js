48790a9a1c55ed860112caafcafcaee7e5e0becc
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-11-26 21:19:21 +0700

    rx

---
+++ nodejs/11.js
@@ -0,0 +1,12 @@
+let user = {}
+Object.defineProperty(user, 'name', {
+  get () {
+    return this.name
+  },
+  set (value) {
+    this.$name = value
+    console.log(`name change to ${value}`)
+  }
+})
+
+user.name = 'pat'
