bbdb7e5151798b2ce72648e96986b56e33c30c76
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-11-29 17:28:01 +0700

    add

--- rust/function/src/main.rs
+++ rust/function/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    print_sum(5, 6);
+    println!("{}", add_one(1));
 }
 
-fn print_sum(x: i32, y: i32) {
-    println!("sum is : {}", x + y);
+fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
