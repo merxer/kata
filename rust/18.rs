1e320b055f1fd49b16a5be82f62b6de60c4ff23f
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-12-01 20:22:28 +0700

    if else

---
+++ rust/18.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let foo = 5;
+    if foo == 5 { 
+        println!("It works");
+    } else if foo == 6 {
+        println!("it's six");
+    }else{
+        println!("It's not five or six");
+    }
+}
