8b1d187c1aa46a3da9b628fbd0442766385ff2cd
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2017-01-20 10:41:37 +0700

    practice

---
+++ go/415.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	cities := make([]string, 2)
+	cities = append(cities, "Tak", "Bangkok")
+
+
+	fmt.Printf("%q\n", cities)
+	fmt.Println(len(cities))
+
+	cities = append(cities, "Surin")
+
+	fmt.Printf("%q\n", cities)
+	fmt.Println(len(cities))
+}
