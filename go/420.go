acaee096dabbd221cdefd57a8b9a15f1e0fbaf3b
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2017-01-20 10:47:12 +0700

    practice

---
+++ go/420.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	cities := map[string]int {
+		"Tak": 1,
+		"Surin": 2,
+	}
+
+	for key, value := range cities {
+		fmt.Printf("%s is number %d\n", key, value)
+	}
+}
