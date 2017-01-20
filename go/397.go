c86a60a71d7d4fd92b456a400ff6f8547e46ffb5
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2017-01-20 10:04:31 +0700

    practice

---
+++ go/397.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type (
+	Event struct {
+		Name string
+	}
+
+	Bootcamp struct {
+		Event
+		Lat, Lon float64
+		Date time.Time
+	}
+)
+
+func main() {
+	event := &Bootcamp{}
+	event.Name = "Go Hackathorn"
+	event.Lat = 34.012836
+	event.Lon = -118.495338
+	event.Date = time.Now()
+
+
+	fmt.Printf("Event (%s) on %s, location (%f, %f)",
+event.Name, event.Date, event.Lat, event.Lon)
+}
