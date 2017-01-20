10746db2d3c38ef02565474a322806626d0cd9c6
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2017-01-20 11:11:15 +0700

    practice

---
+++ go/437.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Firstname, Lastname string
+}
+
+func (u *User) Name() string {
+	return fmt.Sprintf("%s %s", u.Firstname, u.Lastname)
+}
+
+type Namer interface {
+	Name() string
+}
+
+func Greet(n Namer) string {
+	return fmt.Sprintf("Dear %s", n.Name())
+}
+
+func main() {
+	u := &User{"Mat", "Aimonetti"}
+	fmt.Println(Greet(u))
+}
