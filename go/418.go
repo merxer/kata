56936c910efec1f9550a9ec4063c22d9b5fec709
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2017-01-20 10:43:53 +0700

    practice

---
+++ go/418.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var pow =[]int{1,2,4,8,16,32,64,128}
+
+func main() {
+	for _, value := range pow {
+		if value > 16 {
+			break
+		}
+
+		fmt.Printf("%d\n", value)
+	}
+}
