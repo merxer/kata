5c216048caa2130921bb214fe17434ccd5f5bda0
THAWATCHAI SINGNGAM <singgnam.prasert@gmail.com>
2017-01-22 08:09:05 +0700

    struct

---
+++ rust/rust_struct/src/main.rs
@@ -0,0 +1,16 @@
+struct GrayscaleMap {
+    pixels: Vec<u8>,
+    size: (usize, usize)
+}
+fn main() {
+    let width = 1024;
+    let height = 576;
+
+    let image = GrayscaleMap {
+        size: (width, height),
+        pixels: vec![0; width * height]
+    };
+
+    assert_eq!(image.size, (1024, 576));
+    assert_eq!(image.pixels.len(), 1024 * 576);
+}
