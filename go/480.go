08a178ff8b4a42a730585300723d102ca2de2be8
merxer <34d30y@gmail.com>
2017-01-24 14:50:10 +0700

    practice

---
+++ go/480.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type (
+	Event struct {
+		Name string
+	}
+
+	Bootcamp struct {
+		Event
+		Lat, Lon float64
+		Date time.Time
+	}
+)
+
+func main() {
+	event := &Bootcamp{}
+	event.Name = "Go hackathon"
+	event.Lat = 34.01
+	event.Lon = -118.49
+	event.Date = time.Now()
+
+	fmt.Printf("%T(%v)", event, event)
+}
