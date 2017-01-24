dc6812c00fa9284d647402c6a14b478fb73ada46
merxer <34d30y@gmail.com>
2017-01-24 15:07:03 +0700

    practice

---
+++ go/491.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySlice :=[]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(mySlice)
+
+	fmt.Println(mySlice[1:4])
+	fmt.Println(mySlice[:4])
+	fmt.Println(mySlice[4:])
+}
