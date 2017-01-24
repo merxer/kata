676455f0808e45b8205ef002fad3a29b16b3520d
merxer <34d30y@gmail.com>
2017-01-24 11:18:32 +0700

    practice

---
+++ go/470.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	Age int
+}
+
+func newYear(h Human) int {
+	h.Age++
+	return h.Age
+}
+
+func main() {
+	me := Human{Age: 20}
+	fmt.Println(newYear(me))
+	fmt.Println(me.Age)
+}
