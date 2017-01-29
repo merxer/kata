b5dd8b7c27a77d8b3985bc6f4f91d9fcc72b3f3f
merxer <34d30y@gmail.com>
2017-01-30 05:40:37 +0700

    practice

---
+++ go/500.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("When a flower doesn't bloom, ")
+	fmt.Println("you fix the environment in which it grows,")
+	fmt.Println("not the flower")
+	fmt.Println("- Alexander Den Heijer")
+}
