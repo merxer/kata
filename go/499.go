4d06606ed905a0d59b29f4917d6297a97744b011
merxer <34d30y@gmail.com>
2017-01-30 05:37:46 +0700

    practice

---
+++ go/499.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Experience is the hardest kind of teacher.")
+	fmt.Println("It gives you the test first, and the lesson afterward.")
+	fmt.Println("- OSCA WILDE")
+}
