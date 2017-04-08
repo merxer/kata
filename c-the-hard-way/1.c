b09b3d25a592c1d069ba7155ea420db3ca37b8ed
merxer <34d30y@gmail.com>
2017-04-08 21:20:00 +0700

    practice

---
+++ c-the-hard-way/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is comment. */
+int main(int argc, char *argv[]) { 
+  int distance = 100;
+
+  //this is also a comment
+  printf("You are %d miles ways. ", distance);
+
+  return 0;
+}
