76262e5283f2a28466309e57f6ac78f39c8d624c
merxer <34d30y@gmail.com>
2017-05-12 20:28:01 +0700

    practice

--- erlang/drop.erl
+++ erlang/drop.erl
@@ -1,5 +1,16 @@
--module(drop).
--export([fall_velocity/1]).
+%% @author Simon St.Laurent <simonstl@simonstl.com> [http://simonstl.com]
+%% @doc Functions calculating velocities achieved by objects
+%% dropped in a vacuum.
+%% @reference from <a href= "http://shop.oreilly.com/product/0636920025818.do" >
+%% Introducing Erlang</a>,
+%% O'Reilly Media, Inc., 2017.
+%% @copyright 2017 by Simon St.Laurent
+%% @version 0.1
 
-fall_velocity(Distance) -> math:sqrt(2 * 9.8 * Distance).
+-module(drop).
+-export([fall_velocity/2]).
+
+fall_velocity(earth, Distance) -> math:sqrt(2 * 9.8 * Distance);
+fall_velocity(moon, Distance) -> math:sqrt(2 * 1.6 * Distance);
+fall_velocity(mars, Distance) -> math:sqrt(2 * 3.71 * Distance).
 
