23634a4f7327b024162c19d3132f3a9c355778f7
merxer <34d30y@gmail.com>
2017-07-13 23:36:33 +0700

    Construct and Call Random Number

--- java/ch01/sec01/HelloWorld.java
+++ java/ch01/sec01/HelloWorld.java
@@ -1,9 +1,15 @@
 package ch01.sec01;
 
+import java.util.Random;
+
 // Our first Java program
 //
 public class HelloWorld {
   public static void main(String [] args) {
     System.out.println("Hello, World".length());
+
+    Random generator = new Random();
+    System.out.println(generator.nextInt());
+    System.out.println(generator.nextInt());
   }
 }
